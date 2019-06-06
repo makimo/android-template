5ded649ef0e3b892d52d8d23c1ff091217d8e9fe
Iwo Herka <iwo.herka@makimo.pl>
2019-06-06 15:21:33 +0000

    Add Kotlin extension functions PREF-28

--- app/src/main/java/pl/makimo/androidtemplate/BaseActivity.kt
+++ app/src/main/java/pl/makimo/androidtemplate/BaseActivity.kt
@@ -8,7 +8,11 @@ import pl.makimo.androidtemplate.dagger.App
  * Basis for all [Activities]. All [Activities] must inherit from this one.
  */
 open class BaseActivity : AppCompatActivity() {
-
+    /**
+     * Shortcut for Dagger initialization. One can simply do `component().inject(this)`.
+     * "component" here stands for [DaggerAppComponent] which is a Dagger-generated
+     * class injecting the dependencies.
+     */
     val component by lazy { (application as App).component }
 
     override fun onCreate(savedInstanceState: Bundle?) {
